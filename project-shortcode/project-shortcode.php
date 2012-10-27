f42518febd225890b03cc081296e2123d1cadf29
Hallgeir Lien <hallgeir@server9.subsys.no>
2012-10-27 10:29:11 +0200

    Added licence text. Changed licence text in php file.

--- project-shortcode/project-shortcode.php
+++ project-shortcode/project-shortcode.php
@@ -1,25 +1,25 @@
 <?php
 /*
 Plugin Name: Project Shortcodes
-Plugin URI: 
+Plugin URI: http://www.hallgeir.org/programming
 Description: Shortcodes for listing projects in a page.
 Author: Hallgeir Lien
 Version: 1.1
 */
 
-/*  Copyright 2009  Hallgeir Lien
+/*  Copyright 2012  Hallgeir Lien
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
+    it under the terms of the GNU Lesser General Public License as published 
+    by the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Lesser General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
